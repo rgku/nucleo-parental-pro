afb9e0cc5a0a15c2a9e58c172b87e863faf19aba
Núcleo Parental Pro <dev@nucleoparental.pt>
2026-04-12 15:57:05 +0100

    fix: remove middleware completely

--- go.bat
+++ go.bat
@@ -1,4 +1,4 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" add -A
-"C:\Program Files\Git\cmd\git.exe" commit -m "fix: remove vercel.json to avoid conflicts"
+"C:\Program Files\Git\cmd\git.exe" commit -m "fix: remove middleware completely"
 "C:\Program Files\Git\cmd\git.exe" push origin main
