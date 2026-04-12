bca407d02ff3482ceb56f6c8d7a2810e5560a89b
Núcleo Parental Pro <dev@nucleoparental.pt>
2026-04-12 15:50:34 +0100

    fix: add vercel.json config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // trailingSlash: true,
+  trailingSlash: true,
   // PWA support
   headers: async () => [
     {
--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "test: simple home page for debugging 404"
+"C:\Program Files\Git\cmd\git.exe" commit -m "fix: add vercel.json config"
 "C:\Program Files\Git\cmd\git.exe" push origin main
